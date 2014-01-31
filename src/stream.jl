427de337e46820aa3111f9fef5155a85a56da8ef
timholy <tim.holy@gmail.com>
2014-01-31 04:47:15 -0600

    Create NullStream that can be used in wait() (has its own condition)

--- src/stream.jl
+++ src/stream.jl
@@ -16,18 +16,14 @@ type Stream <: AbstractStream
     handle::StreamHandle
     status::Cuint
     c::Condition
-
-    function Stream()
-        p = Array(Ptr{Void}, 1)
-        rt.cudaStreamCreate(p)
-        hnd = StreamHandle(p[1])
-        c = Condition()
-        new(hnd, 0, c)
-    end
 end
-
-type NullStream <: AbstractStream
+function Stream()
+    p = Array(Ptr{Void}, 1)
+    rt.cudaStreamCreate(p)
+    hnd = StreamHandle(p[1])
+    Stream(hnd, 0, Condition())
 end
+NullStream() = Stream(StreamHandle(C_NULL), 0, Condition())
 
 convert(::Type{Ptr{Void}}, s::Stream) = convert(Ptr{Void}, s.handle)
 convert(::Type{Ptr{Void}}, s::NullStream) = C_NULL
@@ -37,6 +33,7 @@ const null_stream = NullStream()
 destroy(s::Stream) = rt.cudaStreamDestroy(s.ptr)
 
 synchronize(s::Stream) = rt.cudaStreamSynchronize(s)
+
 function wait(s::Stream)
     rt.cudaStreamAddCallback(s, streamcb, pointer_from_objref(s), 0)
     println("About to wait")
@@ -46,7 +43,7 @@ function wait(s::Stream)
     rt.checkerror(s.status)
 end
 
-wait(s::NullStream) = rt.cudaDeviceSynchronize()
+# wait(s::NullStream) = rt.cudaDeviceSynchronize()
 
 function callback_notify(hnd, status, data)
     println("In notify")
