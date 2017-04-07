c7eb176cc2ada84ad35e76b446becc35ae30e9eb
Mus M <mus-m@outlook.com>
2017-04-07 11:25:02 -0400

    Update libcudart-6.5.jl

--- src/libcudart-6.5.jl
+++ src/libcudart-6.5.jl
@@ -18,7 +18,7 @@ function checkerror(code::Cuint)
     throw(unsafe_string(cudaGetErrorString(code)))
 end
 
-include("../gen-6.5/gen_libcudart_h.jl")
+include(joinpath(dirname(@__FILE__), "..", "gen-6.5", "gen_libcudart_h.jl")
 
 const cudaError_t = cudaError
 
@@ -40,7 +40,7 @@ if isempty(libcudart)
     error("CUDA runtime API library cannot be found")
 end
 
-include("../gen-6.5/gen_libcudart.jl")
+include(joinpath(dirname(@__FILE__), "..", "gen-6.5", "gen_libcudart.jl")
 
 # # Make cudaSetupArgument more convenient
 # function cudaSetupArgument(arg, size, offset)
@@ -48,8 +48,8 @@ include("../gen-6.5/gen_libcudart.jl")
 # end
 
 # Fix issues stemming from the inability to pass structs as args
-const libwrapcuda = Libdl.find_library(["libwrapcuda"],[joinpath(dirname(@__FILE__), "..", "deps")])
-isempty(libwrapcuda) && error("Cannot find libwrapcuda")
+const libwrapcuda = Libdl.find_library(["libwrapcuda"], [joinpath(dirname(@__FILE__), "..", "deps")])
+isempty(libwrapcuda) && error("cannot find libwrapcuda")
 
 function cudaMalloc3D(p::Array{cudaPitchedPtr,1}, ext::cudaExtent)
     checkerror(ccall((:wrapcudaMalloc3D, libwrapcuda), cudaError_t, (Ptr{cudaPitchedPtr}, Ptr{cudaExtent}), p, &ext))
