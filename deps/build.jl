534f178f7af6e6a5b18eb54bcb55eaa47c7c6d49
Tim Besard <tim.besard@gmail.com>
2017-04-20 11:13:56 +0200

    Merge deps/compile.jl and deps/build.jl.
    This will make it possible to embed results from toolchain discovery and architecture selection in ext.jl.

--- deps/build.jl
+++ deps/build.jl
@@ -45,8 +45,6 @@ end
 
 ## discovery routines
 
-const ext = joinpath(@__DIR__, "ext.jl")
-
 "Return path of CUDA toolkit and the runtime library."
 function find_cuda()
     cudapath_envs = ["CUDA_PATH", "CUDA_HOME", "CUDA_ROOT"] 
@@ -121,13 +119,206 @@ function find_nvml_smi()
     libnvml, nvidiasmi
 end
 
+type Toolchain
+    version::VersionNumber
+    nvcc::String
+    flags::Vector{String}
+end
 
-## main build
+function find_toolchain(cudapath)
+    # Check availability NVCC
+    if !isnull(cudapath)
+        nvcc = joinpath(get(cudapath), "bin", "nvcc") * (is_windows() ? ".exe" : "")
+    elseif !is_windows()
+        try
+            nvcc = chomp(readstring(pipeline(`which nvcc`, stderr=DevNull)))
+        catch ex
+            isa(ex, ErrorException) || rethrow(ex)
+            error("Could not find NVIDIA CUDA compiler (nvcc); consider setting the CUDA_PATH environment variable.")
+        end
+    else
+        error("Could not find NVIDIA CUDA compiler (nvcc); consider setting the CUDA_PATH environment variable.")
+    end
 
-function setup_ext()
+    nvcc_ver = Nullable{VersionNumber}()
+    for line in readlines(`$nvcc --version`)
+        m = match(r"release ([0-9.]+)", line)
+        if m !== nothing
+            nvcc_ver = Nullable(VersionNumber(m.captures[1]))
+        end
+    end
+    isnull(nvcc_ver) && error("Could not parse NVIDIA CUDA compiler version info.")
+    version = get(nvcc_ver)
+
+    flags = [ "--compiler-bindir" ]
+
+    # collect possible hostcc executable names
+    if !is_windows()
+        # Determine host compiler version requirements; Source: "CUDA Getting Started Guide for Linux".
+        hostcc_support = (
+            (v"5.0", v"4.6.4"),
+            (v"5.5", v"4.7.3"),
+            (v"6.0", v"4.8.1"),
+            (v"6.5", v"4.8.2"),
+            (v"7.0", v"4.9.2"),
+            (v"7.5", v"4.9.2"),
+            (v"8.0", v"5.3.1") )
+        if version < hostcc_support[1][1]
+            error("no support for CUDA < $(hostcc_req[1][1])")
+        end
+        hostcc_maxver = Nullable{VersionNumber}()
+        for hostcc in hostcc_support
+            if version == hostcc[1]
+                hostcc_maxver = Nullable(hostcc[2])
+                break
+            end
+        end
+        isnull(hostcc_maxver) && error("Unknown NVIDIA CUDA compiler version $version.")
+
+        # enumerate all possible gcc binary names
+        # NOTE: this is coarse, and might list invalid, non-existing versions
+        hostcc_names = [ "gcc" ]
+        for major in 3:7
+            push!(hostcc_names, "gcc-$major")
+            for minor in 0:9
+                push!(hostcc_names, "gcc-$major.$minor")
+                push!(hostcc_names, "gcc$major$minor")
+            end
+        end
+
+        # Check availability host compiler
+        hostcc_possibilities = []
+        for hostcc in hostcc_names
+            hostcc_path = try
+                chomp(readstring(pipeline(`which $hostcc`, stderr=DevNull)))
+            catch ex
+                !isa(ex, ErrorException) && rethrow(ex)
+                continue
+            end
+
+            verstring = chomp(readlines(`$hostcc_path --version`)[1])
+            m = match(Regex("^$hostcc \\(.*\\) ([0-9.]+)"), verstring)
+            if m === nothing
+                warn("Could not parse GCC version info (\"$verstring\"), skipping this compiler.")
+                continue
+            end
+            hostcc_ver = VersionNumber(m.captures[1])
+
+            if hostcc_ver <= get(hostcc_maxver)
+                push!(hostcc_possibilities, (hostcc_path, hostcc_ver))
+            end
+        end
+        if length(hostcc_possibilities) == 0
+            error("Could not find a suitable host compiler (your NVCC $version needs GCC <= $(get(hostcc_maxver))).")
+        end
+        sort!(hostcc_possibilities; rev=true, lt=(a, b) -> a[2]<b[2])
+        hostcc = hostcc_possibilities[1]
+
+        push!(flags, hostcc[1])
+    else # Windows
+        vc_versions = ["VS140COMNTOOLS", "VS120COMNTOOLS", "VS110COMNTOOLS", "VS100COMNTOOLS"]
+        !any(x -> haskey(ENV, x), vc_versions) && error("Compatible Visual Studio installation cannot be found; Visual Studio 2015, 2013, 2012, or 2010 is required.")
+        vs_cmd_tools_dir = ENV[vc_versions[first(find(x -> haskey(ENV, x), vc_versions))]]
+        hostccbin = joinpath(dirname(dirname(dirname(vs_cmd_tools_dir))), "VC", "bin", Sys.WORD_SIZE == 64 ? "amd64" : "", "cl.exe")
+
+        push!(flags, hostccbin)
+    end
+
+    # determine compilation options
+    arch_envs = ["NVCC_ARCH", "GPU_ARCH", "DEVICE_ARCH"]
+    if any(x -> haskey(ENV, x), arch_envs)
+        arch_envs = arch_envs[map(x -> haskey(ENV, x), arch_envs)]
+        if !all(i -> i == arch_envs[1], arch_envs)
+            warn("GPU compilation options set via environment variables are not equivalent.")
+            arch_env = ENV[first(arch_envs)]
+            warn("selecting nvcc arch value: $(arch_envs).")
+        else
+            arch_env = ENV[first(arch_envs)]
+        end
+        append!(flags, [ "--gpu-architecture", arch_env ])
+    end
+
+    return Toolchain(version, nvcc, flags)
+end
+
+
+## main
+
+"Database of compute capabilities with matching shader model, and initial version of the
+CUDA toolkit supporting this architecture."
+const architectures = [
+    # cap       SM          CUDA
+    # NOTE: CUDA versions only checked starting with v4.0
+    (v"1.0",    "sm_10",    v"4.0"),
+    (v"1.1",    "sm_11",    v"4.0"),
+    (v"1.2",    "sm_12",    v"4.0"),
+    (v"1.3",    "sm_13",    v"4.0"),
+    (v"2.0",    "sm_20",    v"4.0"),
+    (v"2.1",    "sm_21",    v"4.0"),
+    (v"3.0",    "sm_30",    v"4.2"),
+    (v"3.2",    "sm_32",    v"6.0"),
+    (v"3.5",    "sm_35",    v"5.0"),
+    (v"3.7",    "sm_37",    v"6.5"),
+    (v"5.0",    "sm_50",    v"6.0"),
+    (v"5.2",    "sm_52",    v"7.0"),
+    (v"5.3",    "sm_53",    v"7.5"),
+    (v"6.0",    "sm_60",    v"8.0"),
+    (v"6.1",    "sm_61",    v"8.0"),
+    (v"6.2",    "sm_62",    v"8.0") ]
+
+
+"Return the most recent supported architecture for a CUDA device's capability."
+function select_architecture(cap::VersionNumber; cuda=nothing)
+    # devices are compatible with code generated for lower compute models
+    compat_architectures = filter(x -> x[1] <= cap, architectures)
+    if cuda !== nothing
+        compat_architectures = filter(x -> cuda >= x[3], compat_architectures)
+    end
+    if length(compat_architectures) == 0
+        error("No support for requested device or software (compute model <= $cap" *
+              cuda === nothing ? "" : ", CUDA >= $cuda).")
+    end
+    return compat_architectures[length(compat_architectures)][2]
+end
+
+const utilsfile = "utils"
+const libfile = "libwrapcuda"
+
+function build(toolchain, arch)
+    compile_flags = vcat(toolchain.flags, ["--gpu-architecture", arch])
+
+    cd(@__DIR__) do
+        rm("$(libfile).$(Libdl.dlext)", force=true)
+        is_windows() && rm("$(libfile).exp", force=true)
+        rm("$(utilsfile).ptx", force=true)
+
+        run(pipeline(`$(toolchain.nvcc) $(compile_flags) --shared wrapcuda.c -o $(libfile).$(Libdl.dlext)`; stdout=DevNull, stderr=STDERR))
+        run(pipeline(`$(toolchain.nvcc) $(compile_flags) -ptx $(utilsfile).cu`; stdout=DevNull, stderr=STDERR))
+    end
+
+    cd(joinpath(@__DIR__, "..", "test")) do
+        rm("vadd.ptx", force=true)
+        run(pipeline(`$(toolchain.nvcc) $(compile_flags) -ptx vadd.cu`; stdout=DevNull, stderr=STDERR))
+    end
+
+    nothing
+end
+
+const ext = joinpath(@__DIR__, "ext.jl")
+
+function main()
+    # discover stuff
     cudapath, libcudart_path = find_cuda()
     libnvml_path, nvidiasmi_path = find_nvml_smi()
+    toolchain = find_toolchain(cudapath)
 
+    # select a compatible architecture and build code for it
+    caps = [capability(libcudart_path, i) for i in 0:devcount(libcudart_path)-1]
+    compat_cap = reduce((a,b)->a<b?a:b, caps)
+    compat_arch = select_architecture(compat_cap; cuda=toolchain.version)
+    build(toolchain, compat_arch)
+
+    # gather some additional info
     libcudart_version = version(libcudart_path)
 
     # check if we need to rebuild
@@ -144,10 +335,8 @@ function setup_ext()
     end
 
     # write ext.jl
-    home = isnull(cudapath) ? "" : escape_string(get(cudapath))
     open(ext, "w") do fh
         write(fh, """
-            const home = "$home"
             const libcudart_path = "$(escape_string(libcudart_path))"
             const libcudart_version = v"$libcudart_version"
 
@@ -159,12 +348,9 @@ function setup_ext()
 end
 
 try
-    setup_ext()
+    main()
 catch ex
     # if anything goes wrong, wipe the existing ext.jl to prevent the package from loading
     rm(ext; force=true)
     rethrow(ex)
 end
-
-include(ext)
-include("compile.jl")
